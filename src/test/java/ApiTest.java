0c20d64ae1ec4448ea2dba52cea1432735c88528
Mithun Kumar Mishra <mithunkumarmishra@Mithuns-MacBook-Pro.local>
2026-05-05 22:14:36 -0700

    Initial commit

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,29 +1,33 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ApiTest {
 
     @Test
-    public void createAdminTest() {
+    public void createUserTest() {
 
         String data = """
         {
-            "email": "abcdmm12388@gmail.com",
-            "password": "1234567@mMMa",
-            "role": "ADMIN"
+            "name": "John Doe",
+            "job": "QA Engineer"
         }
         """;
 
         Response resp = RestAssured
                 .given()
-                .relaxedHTTPSValidation()  // <-- Ignore SSL issues
+                .baseUri("https://reqres.in")
+                .basePath("/api/users")
                 .header("Content-Type", "application/json")
+                .header("x-api-key", "free_user_3DKaWQw5d42nZdIkmpyPaBo6SJf")
                 .body(data)
                 .when()
-                .post("https://www.shoppersstack.com/shopping/POST/users/login");
+                .post();
 
         System.out.println("Status Code: " + resp.getStatusCode());
         System.out.println("Response Body: " + resp.getBody().asString());
+
+        //Assert.assertEquals(resp.getStatusCode(), 201);
     }
 }
